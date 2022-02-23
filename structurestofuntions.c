5f61d7d808ebe7c20b73ff03b7dea256060f9e19
sangeeth-666 <98574027+sangeeth-666@users.noreply.github.com>
2022-02-23 21:48:59 +0530

    Update structurestofuntions.c

--- structurestofuntions.c
+++ structurestofuntions.c
@@ -1,5 +1,5 @@
-/*#include<stdio.h>
-struct student func1(struct student ); // function declaration
+#include<stdio.h>
+struct student func1(struct student ); 
 struct student
 {
     char name[30];    
@@ -11,29 +11,9 @@ int main()
     func1(s1); // function call
     return 0;
 }
-//...........function definition.........
+
 struct student func1(struct student s)
 {
     printf("Name: %s\nRoll no: %d\n",s.name,s.roll);
-}*/
-
-/*--------call by reference------*/
-#include<stdio.h>
-struct student func1(struct student *s); // function declaration
-struct student
-{
-    char name[30];    
-	int roll;
-};
-int main()
-{
-    struct student s1={"sangeeth",411};    
-	func1(&s1); // function call    
-	return 0;
-}
-/*...........function definition.........*/
-struct student func1(struct student *s)
-{
-    printf("Name: %s\nRoll no: %d\n",s->name,s->roll);
 }
 
